9c954ccc141efb0927e4dcf6b78d16bf3d275950
avi8779 <avi87793919@gmail.com>
2023-12-19 23:22:51 +0530

    final tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Grid from './component/Grid/Grid'
+
+function App() {
+  return (
+    <>
+     <Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App
